0097e00021b81dab0f44e0d7e2c88db309b8690f
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-01-24 23:34:07 +0100

    changed json files to support multiple ports

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 const professorRouter = require('./routes/professorRoute');
@@ -37,6 +37,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+
+app.use(cors());
 
 
 app.use(session({
